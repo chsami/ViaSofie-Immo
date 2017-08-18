94f71f560d7ac7f56cce275fafc84667762c6835
AmaraKromah <amara-kromah@hotmail.com>
2017-08-18 22:19:19 +0200

    oeps

--- ViaSofie-master/webapp/context_processor.py
+++
@@ -1,6 +0,0 @@
-from webapp.models import *
-def header(request):
-    cover = Data.objects.get(id=10)
-    return {
-        'conttest': "conttest",
-    }
--- ViaSofie-master/webapp/templatetags/module_sander.py
+++
@@ -1,8 +0,0 @@
-from django import template
-register = template.Library()
-from webapp.models import *
-
-@register.simple_tag
-def get_obj_data(pk, attr):
-    obj = getattr(Data.objects.get(pk=int(pk)), attr)
-    return obj
